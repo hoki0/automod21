a09999642f06719acf27daf426d97256fae0051d
rahh <67960695+hoki0@users.noreply.github.com>
2021-03-21 23:11:05 -1200

    Update supportserver.js

--- src/commands/info/supportserver.js
+++ src/commands/info/supportserver.js
@@ -7,17 +7,17 @@ module.exports = class SupportServerCommand extends Command {
       name: 'supportserver',
       aliases: ['support', 'ss'],
       usage: 'supportserver',
-      description: 'Displays the invite link to Any Bot\'s Discord Support Server.',
+      description: 'Displays the invite link to Fluxed Bot\'s Discord Support Server.',
       type: client.types.INFO
     });
   }
   run(message) {
     const embed = new MessageEmbed()
       .setTitle('Support Server')
-      .setThumbnail('https://cdn.glitch.com/5bfb504c-974f-4460-ab6e-066acc7e4fa6%2Fezgif.com-gif-to-apng.png?v=1595260265531')
-      .setDescription('Click [here](https://discord.gg/2FRpkNr) to join the Any Bot Support Server!')
+      .setThumbnail('https://media.tenor.com/images/36d4a3cca93e9afe73bcb8e8bb70a69a/tenor.gif')
+      .setDescription('Click [here](https://discord.gg/vcTwsbREQB) to join the Fluxed  Bot Support Server!')
       .addField('Other Links', 
-        '**[Invite Me](https://discordapp.com/oauth2/authorize?client_id=733728002910715977&scope=bot&permissions=8)**'
+        '**[Invite Me](https://discord.com/api/oauth2/authorize?client_id=712313863256670218&permissions=8&scope=bot)**'
       )
       .setFooter(message.member.displayName,  message.author.displayAvatarURL({ dynamic: true }))
       .setTimestamp()
