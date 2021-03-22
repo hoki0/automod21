586e1f0d5b208b4fccbbdf925db3e72fc5fd44b7
rahh <67960695+hoki0@users.noreply.github.com>
2021-03-21 22:50:22 -1200

    Update setcommandpoints.js

--- src/commands/admin/setcommandpoints.js
+++ src/commands/admin/setcommandpoints.js
@@ -8,7 +8,7 @@ module.exports = class SetCommandPointsCommand extends Command {
       name: 'setcommandpoints',
       aliases: ['setcp', 'scp'],
       usage: 'setcommandpoints <point count>',
-      description: 'Sets the amount of points earned per Any Bot command used.',
+      description: 'Sets the amount of points earned per Fluxed Bot command used.',
       type: client.types.ADMIN,
       userPermissions: ['MANAGE_GUILD'],
       examples: ['setcommandpoints 5']
