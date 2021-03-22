15b987d00da546d043ce23207b6c0a9922981757
rahh <67960695+hoki0@users.noreply.github.com>
2021-03-21 22:42:38 -1200

    Update confgi.js

--- src/commands/anti/confgi.js
+++ src/commands/anti/confgi.js
@@ -1,1 +1,17 @@
+const Command = require('../Command.js');
+const { MessageEmbed } = require('discord.js');
+const fetch = require('node-fetch');
 
+module.exports = class ConfigCommand extends Command {
+  constructor(client) {
+    super(client, {
+      name: 'config',
+      usage: 'config',
+      description: 'Shows the antinuke configs ',
+      examples: [],
+      type: client.types.ANTI
+    });
+  }
+  async run(message) {
+    }
+  };
